c90e5978828d805b9c2cf2358c3f0268d8d365d2
Bar Horing <bar.apple@gmail.com>
2018-10-08 17:12:09 +0300

    more components

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is a title.'},
-    'Hello world!'
+  'h1',
+  { id: 'main-title', title: 'This is a title.'},
+  'Hello world!'
 );
 
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned how to create a react node and render it tp the dom'
+);
+
+const header = React.createElement(
+  'header',
+  null,
+  title,
+  desc
+)
+
+
+
 ReactDOM.render(
-    title,
-    document.getElementById('root')
+  header,
+  document.getElementById('root')
 );
 
 console.log(title);
