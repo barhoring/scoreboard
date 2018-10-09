a021524f86c58818ad70706a78c473e2c8f9260d
Bar Horing <bar.apple@gmail.com>
2018-10-09 11:38:54 +0300

    playing with jsx

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const title = <h1>Hello world!</h1>;
-const desc = <p>I just learned how to create a react node and render it tp the dom</p>;
-
-const header = React.createElement(
-  'header',
-  null,
-  title,
-  desc
+const desc = 'I just learned how to create a react node and render it tp the dom';
+const myTitleID ='main-title';
+const name = 'Bar';
+const header = (
+  <header>
+    <h1 id={myTitleID}>{ name }'s 100 React element</h1>
+    <p className="main-desc">{ desc }</p>
+    <input value={10*20} />
+  </header>
 );
 
 
@@ -16,5 +17,3 @@ ReactDOM.render(
   header,
   document.getElementById('root')
 );
-
-console.log(title);
