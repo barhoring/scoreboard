b0da47e017fe7441600ff8e8d45dabcac1b8abf9
Bar Horing <bar.apple@gmail.com>
2018-10-13 23:15:39 +0300

    now with decrementScore

--- app.js
+++ app.js
@@ -61,11 +61,15 @@ class Counter extends React.Component {
   incrementScore = () => {
     this.setState({score: this.state.score + 1});
   };
+ 
+  decrementScore = () => {
+    this.setState({score: this.state.score - 1});
+  };
 
   render() {
     return (
       <div className="counter">
-        <button className="counter-action decrement"> - </button>
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
         <span className="counter-score">{this.state.score}</span>
         <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
